199c3b6b0b1bea103416dc6785e0f22a286ac757
Ahmedfci378 <ahmedfci378@gmail.com>
2026-03-08 01:26:31 +0200

    updated

--- src/services/project.service.js
+++ src/services/project.service.js
@@ -4,7 +4,7 @@ exports.getActiveProjects = async () => {
   return await Project.find({ active: true });
 };
 
-exports.getProjectsByGoal = async (goal) => {
+exports.getProjectByGoal = async (goal) => {
   return await Project.find({
     type: goal,
     active: true
