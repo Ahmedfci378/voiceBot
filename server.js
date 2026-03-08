80245d33e99a1c357eba285a08c102194fbed67e
Ahmedfci378 <ahmedfci378@gmail.com>
2026-03-08 12:05:33 +0200

    updated

--- server.js
+++ server.js
@@ -2,24 +2,19 @@ const app = require("./src/app");
 const http = require("http");
 const { Server } = require("socket.io");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 const server = http.createServer(app);
 
-// إعداد Socket.io
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:3000",
-      "https://voice-bot-front.vercel.app"
-    ],
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
 
-// ربط ملف الـ sockets
 require("./src/sockets/voice.socket")(io);
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port", PORT);
 });
