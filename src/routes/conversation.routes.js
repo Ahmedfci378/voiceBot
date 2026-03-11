5cb6012d23e8399e5af7eacdee8f6f39c413e620
Ahmedfci378 <ahmedfci378@gmail.com>
2026-03-11 17:18:25 +0200

    updated

--- src/routes/conversation.routes.js
+++ src/routes/conversation.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Conversation = require("../models/conversation.model");
 
-router.get("/:sessionId", async (req, res) => {
+router.get("/", async (req, res) => {
 
   try {
 
